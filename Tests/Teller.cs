f79bcf9775bcff29e1c4e3037c6cb99f6e3673bb
Millie Silva <milliem@tuta.io>
2022-07-15 02:18:33 +0100

    [Solution] Fixed Tests referencing

---
+++ Tests/Teller.cs
@@ -0,0 +1,55 @@
+﻿// System Imports
+
+// Library Imports
+using Library.Network;
+using Library.Network.Teller;
+
+// External Imports
+using Xunit;
+
+
+namespace Tests
+{
+    public class Teller
+    {
+        [Fact]
+        public TellerWorker TestWorker()
+        {
+            var worker = new TellerWorker(new()
+            {
+                IPv4 = "localhost",
+                Port = Constants.DefaultWorkerServerPort
+            });
+
+            return worker;
+        }
+
+        [Fact]
+        public TellerWatcher TestWatcher()
+        {
+            var watcher = new TellerWatcher();
+
+            return watcher;
+        }
+
+        [Fact]
+        public void TestWorkerWatcherConnection()
+        {
+            var worker = new TellerWorker(new()
+            {
+                IPv4 = "localhost",
+                Port = 51002
+            });
+
+            worker.Listen();
+
+            var watcher = new TellerWatcher();
+            watcher.Connect(worker);
+
+            var directory = @"C:\Users\TechMech\Desktop";
+            // var tree = watcher.RPCClient?.ListWorkerDirectory(new Directory { Directory_ = directory });
+
+            // Assert.True(tree?.Tree.Count > 0);
+        }
+    }
+}
