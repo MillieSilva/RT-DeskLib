088eebff374dbfd6eaeef53279edf3c3cab65291
Millie Silva <milliem@tuta.io>
2022-07-19 14:22:44 +0100

    [Library] Proper Broker address for build configurations

--- Network/Constants.cs
+++ Network/Constants.cs
@@ -1,14 +1,23 @@
-﻿// SYsten Unoirts
+﻿// System Imports
 
 // Library Imports
 
 // External Imports
+using Library.Network.Broker;
 
 
-namespace Library.Network
+namespace Library.Network;
+
+public class Constants
 {
-    public class Constants
+    public const ushort DefaultWorkerServerPort = 51001;
+
+    public static string? GetBrokerAddress()
     {
-        public const ushort DefaultWorkerServerPort = 51001;
+#if DEBUG
+        return "http://127.0.0.1:5000";
+#else
+        return Metadata.GetValidBrokerAddress()?.ToString();
+#endif
     }
 }
